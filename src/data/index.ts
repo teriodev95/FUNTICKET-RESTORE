477973bdc6235c99478be2e157a0c957a5d99652
sahidAle <sahidaledev@gmail.com>
2024-07-07 19:00:02 -0600

    fix(): error en datos no usados

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,20 @@
-export const mainEvents = [
+interface Event { 
+  title: string;
+  events: {
+    ciudad: string;
+    fecha: string;
+    lugar: string;
+    urlboton: string;
+    urlfoto: string;
+    links: {
+      label: string;
+      url: string;
+    }[];
+  }[];
+
+}
+
+export const mainEvents: Event[] = [
   {
     title: "Aventura - Cerrando Ciclos",
     events: [
@@ -7,35 +23,40 @@ export const mainEvents = [
         fecha: "14 ago 2024",
         lugar: "Estadio Akron",
         urlboton: "https://boletos.funticket.mx/boletos28/public/janto/main.php?Nivel=Evento&idEvento=AVENTGDL",
-        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_GDL_Proximos.jpeg"
+        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_GDL_Proximos.jpeg",
+        links: []
       },
       {
         ciudad: "Mérida",
         fecha: "25 ago 2024",
         lugar: "ESTADIO CARLOS ITURRALDE",
         urlboton: "https://boletos.funticket.mx/boletos33/public/janto/main.php?Nivel=Evento&idEvento=AVENTURAMER",
-        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_MER_Proximos.jpeg"
+        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_MER_Proximos.jpeg",
+        links: []
       },
       {
         ciudad: "Monterrey",
         fecha: "20 ago 2024",
         lugar: "Estadio Banorte",
         urlboton: "https://boletos.funticket.mx/boletos31/public/janto/main.php?Nivel=Evento&idEvento=AVMTY",
-        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_MTY_Proximos.jpeg"
+        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_MTY_Proximos.jpeg",
+        links: []
       },
       {
         ciudad: "Puebla",
         fecha: "23 ago 2024",
         lugar: "CENTRO EXPOSITOR LOS FUERTES",
         urlboton: "https://boletos.funticket.mx/boletos32/public/janto/main.php?Nivel=Evento&idEvento=AVPUEBLA",
-        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_PUE_Proximos.jpeg"
+        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_PUE_Proximos.jpeg",
+        links: []
       },
       {
         ciudad: "Querétaro",
         fecha: "16 ago 2024",
         lugar: "ESTADIO CORREGIDORA",
         urlboton: "https://boletos.funticket.mx/boletos29/public/janto/main.php?Nivel=Evento&idEvento=AVEQRO",
-        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_QRO_Proximos.jpeg"
+        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_QRO_Proximos.jpeg",
+        links: []
       }
     ]
   }
