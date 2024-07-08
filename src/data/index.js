ba734b82c8972b0ff10d20915dc9e18a41859783
sahidAle <sahidaledev@gmail.com>
2024-07-07 18:50:12 -0600

    add(): Eventos de aventura

--- src/data/index.js
+++ src/data/index.js
@@ -1,66 +1,46 @@
-// data/index.js
-
 export const mainEvents = [
   {
-    title: "BamBam - The 1st. World Tour AREA 52",
+    title: "Aventura - Cerrando Ciclos",
     events: [
+      {
+        ciudad: "Guadalajara",
+        fecha: "14 ago 2024",
+        lugar: "Estadio Akron",
+        urlboton: "https://boletos.funticket.mx/boletos28/public/janto/main.php?Nivel=Evento&idEvento=AVENTGDL",
+        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_GDL_Proximos.jpeg"
+      },
+      {
+        ciudad: "Mérida",
+        fecha: "25 ago 2024",
+        lugar: "ESTADIO CARLOS ITURRALDE",
+        urlboton: "https://boletos.funticket.mx/boletos33/public/janto/main.php?Nivel=Evento&idEvento=AVENTURAMER",
+        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_MER_Proximos.jpeg"
+      },
       {
         ciudad: "Monterrey",
-        fecha: "10 mar 2024",
-        lugar: "Gimnasio de Nuevo León",
-        urlboton: "https://boletos.funticket.mx/boletos7/public/janto/main.php?Nivel=Evento&idEvento=BAMBAMNTY",
-        urlfoto: "https://imgcdn.terio.xyz/images/2024/01/17/FunTicket-BamBam_VG_Reco.webp",
-        links: [
-          {
-            label: "HI TOUCH + MINI TALK",
-            link: "https://boletos.funticket.mx/boletos7/public/janto/main.php?Nivel=Evento&idEvento=BAMNLHI"
-          },
-          {
-            label: "MINI TALK",
-            link: "https://boletos.funticket.mx/boletos7/public/janto/main.php?Nivel=Evento&idEvento=BAMNLMINI"
-          }
-        ]
-      }
-    ]
-  },
-  {
-    title: "Ana Gabriel - Tour 2024",
-    events: [
-      {
-        ciudad: "Torreón",
-        fecha: "14 marzo 2024",
-        links: [],
-        lugar: "Estadio Revolución - Torreón",
-        urlboton: "https://boletos.funticket.mx/boletos30/public/janto/main.php?Nivel=Evento&idEvento=AG24TORR",
-        urlfoto: "https://imgcdn.terio.xyz/images/2023/12/15/FunTicket-AnaGabriel_PREV_Reco_Torreon.jpeg"
+        fecha: "20 ago 2024",
+        lugar: "Estadio Banorte",
+        urlboton: "https://boletos.funticket.mx/boletos31/public/janto/main.php?Nivel=Evento&idEvento=AVMTY",
+        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_MTY_Proximos.jpeg"
       },
       {
         ciudad: "Puebla",
-        fecha: "22 marzo 2024",
-        links: [],
-        lugar: "Estadio Hermanos Serdán - Puebla",
-        urlboton: "https://boletos.funticket.mx/boletos29/public/janto/main.php?Nivel=Evento&idEvento=AG24PUE",
-        urlfoto: "https://imgcdn.terio.xyz/images/2023/12/15/FunTicket-AnaGabriel_PREV_Reco_Puebla.jpeg"
+        fecha: "23 ago 2024",
+        lugar: "CENTRO EXPOSITOR LOS FUERTES",
+        urlboton: "https://boletos.funticket.mx/boletos32/public/janto/main.php?Nivel=Evento&idEvento=AVPUEBLA",
+        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_PUE_Proximos.jpeg"
       },
       {
-        ciudad: "Cancún",
-        fecha: "24 marzo 2024",
-        links: [],
-        lugar: "Estadio Beto Ávila - Cancún",
-        urlboton: "https://boletos.funticket.mx/boletos31/public/janto/main.php?Nivel=Evento&idEvento=AG24CAN",
-        urlfoto: "https://imgcdn.terio.xyz/images/2023/12/15/FunTicket-AnaGabriel_PREV_Reco_Cancun.jpeg"
-      },
-      {
-        ciudad: "León",
-        fecha: "18 abril 2024",
-        links: [],
-        lugar: "Velaria - León",
-        urlboton: "https://boletos.funticket.mx/boletos32/public/janto/main.php?Nivel=Evento&idEvento=AG24LEON",
-        urlfoto: "https://imgcdn.terio.xyz/images/2023/12/15/FunTicket-AnaGabriel_PREV_Reco_Leon.jpeg"
+        ciudad: "Querétaro",
+        fecha: "16 ago 2024",
+        lugar: "ESTADIO CORREGIDORA",
+        urlboton: "https://boletos.funticket.mx/boletos29/public/janto/main.php?Nivel=Evento&idEvento=AVEQRO",
+        urlfoto: "https://imgcdn.terio.xyz/images/2024/07/07/FunTicket-Aventura_PrevBBVA_QRO_Proximos.jpeg"
       }
     ]
   }
-]
+];
+
 
 export const footer = {
   whatsapp: "+52 56 1200 8914",
